71dc41f327de4a7d2963ba36b91e837d779b03ea
duyen26912 <109296571+duyen26912@users.noreply.github.com>
2023-12-09 16:02:31 +0700

    b2 Tuan 5

---
+++ src/Tuan5/b2.java
@@ -0,0 +1,69 @@
+package Tuan5;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class b2 {
+
+    /*
+     * Complete the 'isBalanced' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String isBalanced(String s) {
+        // Write your code here
+        Stack<Character> stack = new Stack<>();
+
+        for(int i=0; i<s.length(); i++) {
+            if(s.charAt(i) == '(') {
+                stack.push(')');
+            } else if(s.charAt(i) == '{'){
+                stack.push('}');
+            } else if (s.charAt(i) == '[') {
+                stack.push(']');
+            } else if( stack.isEmpty() || s.charAt(i) != stack.pop() ) {
+                return "NO";
+            }
+        }
+
+        return stack.isEmpty() ? "YES" : "NO";
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                String result = b2.isBalanced(s);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
