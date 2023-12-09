eccd79cf7b1985bb8731a7653016f375982633bb
duyen26912 <109296571+duyen26912@users.noreply.github.com>
2023-12-09 16:06:53 +0700

    b5 Tuan 5

---
+++ src/Tuan5/b5.java
@@ -0,0 +1,95 @@
+package Tuan5;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'equalStacks' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY h1
+     *  2. INTEGER_ARRAY h2
+     *  3. INTEGER_ARRAY h3
+     */
+
+    public static int equalStacks(List<Integer> h1, List<Integer> h2, List<Integer> h3) {
+        // Write your code here
+        int m1 = sum(h1);
+        int m2 = sum(h2);
+        int m3 = sum(h3);
+
+        Collections.reverse(h1);
+        Collections.reverse(h2);
+        Collections.reverse(h3);
+
+        while (!(m1 == m2 && m2 == m3) && (m1 * m2 * m3 != 0)) {
+            int m = Math.max(Math.max(m1, m2), m3);
+            if (m == m1) {
+                m1 -= h1.remove(h1.size() - 1);
+            } else if (m == m2) {
+                m2 -= h2.remove(h2.size() - 1);
+            } else {
+                m3 -= h3.remove(h3.size() - 1);
+            }
+        }
+
+        return Math.min(Math.min(m1, m2), m3);
+    }
+
+    private static int sum(List<Integer> list) {
+        int sum = 0;
+        for (int num : list) {
+            sum += num;
+        }
+        return sum;
+    }
+
+}
+
+public class b5 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n1 = Integer.parseInt(firstMultipleInput[0]);
+
+        int n2 = Integer.parseInt(firstMultipleInput[1]);
+
+        int n3 = Integer.parseInt(firstMultipleInput[2]);
+
+        List<Integer> h1 = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> h2 = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> h3 = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.equalStacks(h1, h2, h3);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
