1882849fe89895063c62c60bdb4aa11013a22bf5
duyen26912 <109296571+duyen26912@users.noreply.github.com>
2023-12-10 21:20:20 +0700

    b2 Tuan7

---
+++ src/Tuan7/b2.java
@@ -0,0 +1,64 @@
+package Tuan7;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'quickSort' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static List<Integer> quickSort(List<Integer> arr) {
+        // Write your code here
+        int pivot = arr.get(0);
+        int low =0;
+        int high = arr.size();
+        while(low<high){
+            do{low++;} while(arr.get(low) < pivot);
+            do{high--;} while(arr.get(high)>pivot);
+            if(low<high) Collections.swap(arr,low,high);
+        }
+        Collections.swap(arr, 0, high);
+        return arr;
+
+    }
+
+}
+
+public class b2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = Result.quickSort(arr);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
